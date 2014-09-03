3a7749436a57a5645139308ede62ba1f536243f7
Ryan Grannell <rgrannell1@users.noreply.github.com>
2014-09-03 12:35:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 git clone https://github.com/rgrannell1/tin
 cd tin
-echo alias polo=\'$(pwd -P)/lib/tin.js\' >> ~/.bashrc && . ~/.bashrc
+echo alias tin=\'$(pwd -P)/lib/tin.js\' >> ~/.bashrc && . ~/.bashrc
